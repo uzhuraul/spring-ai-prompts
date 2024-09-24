60e119b4e5554ca06ce7fb0d8362b6d218efe510
uzhuraul <uladzislau.zhurauliou@gmail.com>
2024-09-24 14:29:05 +0200

    Init commit

---
+++ src/main/java/uladzislau/zhurauliou/spring_ai_prompt/SpringAiPromptApplication.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_prompt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPromptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiPromptApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uladzislau/zhurauliou/spring_ai_prompt/SpringAiPromptApplicationTests.java
@@ -0,0 +1,13 @@
+package uladzislau.zhurauliou.spring_ai_prompt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPromptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
